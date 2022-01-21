1256168bad3e98565cfe8890458cd30f0d782e6d
RaimondasR <raimondas.r2@gmail.com>
2022-01-21 12:55:58 +0200

    Code updated.

---
+++ src/Pages/MainPage.js
@@ -0,0 +1,11 @@
+import React, {useEffect} from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/Pages/StartPage.js
@@ -0,0 +1,11 @@
+import React, { useEffect} from 'react';
+
+const StartPage = (characters, setCharacters) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default StartPage;
